e507204271b3a9633006658ea29de6ea6171fb10
Yuliia Topalova <yuliia.topalova@gmail.com>
2021-11-10 10:42:33 +0000

    commit 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: :new_article
+  post '/articles', to: 'articles#create'
+  get '/articles/:id', to: 'articles#show', as: :article
+  delete '/articles/:id', to: 'articles#destroy'
 end
